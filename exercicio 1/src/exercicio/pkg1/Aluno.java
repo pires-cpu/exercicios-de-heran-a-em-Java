5c7822b09cc3adaceb0b1c7b217e62a9ec821f3e
Aluno São Lucas <aluno.saolucas@ensino.ulbra.net>
2026-03-13 16:21:14 -0300

    atividade 1

---
+++ exercicio 1/src/exercicio/pkg1/Aluno.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio.pkg1;
+
+/**
+ *
+ * @author aluno.saolucas
+ */
+public class Aluno {
+      private String nome;
+    private int idade;
+    
+    public Aluno(String nome, int idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+    
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+    
+    public String getNome(){
+        return nome;
+    }
+    
+    public int getIdade(){
+        return idade;
+    }
+    
+    public void getStatus1(){
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+    }
+
+    
+}
---
+++ exercicio 1/src/exercicio/pkg1/Exercicio1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio.pkg1;
+
+/**
+ *
+ * @author aluno.saolucas
+ */
+public class Exercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+          Aluno objaluno = new Aluno("Rafael Paffrath", 17, "Informatica", "2022321");
+        objaluno.getStatus1();
+        objaluno.getStatus2();
+
+    }
+    
+}
